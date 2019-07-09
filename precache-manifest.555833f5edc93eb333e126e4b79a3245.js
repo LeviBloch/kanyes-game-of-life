89851b32ee20322126380659fa8ffe34de744939
LeviBloch <levi.bloch@gmail.com>
2019-07-09 17:35:04 -0400

    moved things around

---
+++ precache-manifest.555833f5edc93eb333e126e4b79a3245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7547616721f1491bacfa37fa5ea9f383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a82965bf4ffa437c64a",
+    "url": "/static/css/main.f30f3387.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "8a82965bf4ffa437c64a",
+    "url": "/static/js/main.634aaf12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
